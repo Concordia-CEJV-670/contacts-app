23d957c6d92a4bb9819d2f5f1781f96bde949e0e
PranavBhatia <bhatiapranav1996@gmail.com>
2024-03-12 11:18:12 -0400

    GET all contacts

--- src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ltp.contacts.service;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,11 @@ public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
+
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
+    }
 
     @Override
     public Contact getContactById(String id) {
