a5a8d0d9b5c02a73ba91f779531bdac92a92f7fc
PranavBhatia <bhatiapranav1996@gmail.com>
2024-03-12 11:14:45 -0400

    DELETE Request

--- src/main/java/com/ltp/contacts/web/ContactController.java
+++ src/main/java/com/ltp/contacts/web/ContactController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,12 @@ public class ContactController {
     public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
         contactService.updateContact(id, contact);
         return new ResponseEntity<>(contactService.getContactById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("contacts/{id}")
+    public ResponseEntity<Contact> deleteContact(@PathVariable String id) {
+        contactService.deleteContact(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
